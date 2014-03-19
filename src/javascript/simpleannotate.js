d525d3abafcab27e6fa310901b604ae2abcf8473
Reinier Ladan <reinierl@gmail.com>
2014-03-19 17:06:44 +0100

    Bugfix

--- src/javascript/simpleannotate.js
+++ src/javascript/simpleannotate.js
@@ -1,12 +1,10 @@
 $( document ).ready(function() {
 	if($('.annotation').length == 0)
 	{
-		$("body").append('<div class="meta-info-button"><a href="#meta"><img src="/images/simpleannotate-open.svg"></a></div>');
-
 		$(".meta-info-button").hide();
 	} else {
+		$("body").append('<div class="meta-info-button"><a href="#meta"><img src="/images/simpleannotate-open.svg"></a></div>');
 		$(".meta-info-button a").click(function(){
-
 			if($(this).hasClass('show-annotations')){
 				$(this).removeClass('show-annotations');
 				$(this).find('img').attr('src','/images/simpleannotate-open.svg');
@@ -16,7 +14,6 @@ $( document ).ready(function() {
 				$(this).find('img').attr('src','/images/simpleannotate-close.svg');
 				$(".annotation").show();
 			}
-
 			$('html, body').animate({
 				scrollTop: $(".annotation").first().offset().top -75
 			}, 400);
